6bf9cef15a36817bb1474e2886a5786f836d9e06
Getty708 <yesyes708@gmail.com>
2018-09-03 01:16:13 +0900

    add new app - api

--- django/app/app/urls.py
+++ django/app/app/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dashboard/', include('dashboard.urls'), name='dashboard'),    
+    path('dashboard/', include('dashboard.urls'), name='dashboard'),
+    path('api/', include('api.urls'), name='api'),
 ]
