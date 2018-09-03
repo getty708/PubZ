a207d110e78b3f788e418fc6880947091304f0d3
Getty708 <yesyes708@gmail.com>
2018-09-04 01:37:39 +0900

    Stard implementation of registration form

--- django/app/dashboard/urls.py
+++ django/app/dashboard/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from dashboard import views
+from dashboard import views, views_man
 
 app_name = 'dashboard'
 urlpatterns = [
     # ex: /polls/
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('manage/', views_man.IndexView.as_view(), name='management'),
 ]
