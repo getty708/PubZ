5298a4f91fede4b02aefa871a1ab7738c42b2d12
Getty708 <yesyes708@gmail.com>
2018-10-02 00:21:49 +0900

    Update BibtexFormat function ==> class

--- django/app/dashboard/templatetags/utils_bib_format.py
+++ django/app/dashboard/templatetags/utils_bib_format.py
@@ -4,123 +4,194 @@ from django.utils.safestring import mark_safe
 from django import template
 register = template.Library()
 
-
+from core.models import Author, AuthorOrder
 
 """
 Bibtex Format Filter
 """
 # ==================
+#  Base Class
+# ==================
+class BibtexFormatBase(object):
+    def __init__(self, bibtex, *args, **kwargs):
+        self.bibtex = bibtex
+        self.style = bibtex.book.style
+        self.url_bib = reverse("dashboard:bib_detail", kwargs={'pk':bibtex.id})
+        
+
+    def __call__(self):
+        # Convert Bibtex object into dict
+        bibtex = self.bibtex
+        context = bibtex.__dict__
+        context['title'] = bibtex.title_en if bibtex.title_en else bibtex.title_ja
+        context['url_bib'] = self.url_bib
+        context['book_title'] = bibtex.book.title
+        context['year'] = bibtex.pub_date.year
+        context['publisher'] = bibtex.book.publisher
+        # Get Authors
+        authors = AuthorOrder.objects.filter(bibtex=bibtex).order_by('order')
+        context["authors"] = self.get_html_authors(authors)
+        # Fill Placeholders
+        html_template = self.get_template()
+        html = html_template.format(**context)
+        return mark_safe(html)
+    
+    
+    def get_template(self,):
+        try:
+            html = eval('self.get_template_{}()'.format(self.style))
+        except AttributeError:
+            html = self.get_template_DEFAULT()
+        return html
+
+    #
+    #  Template 
+    def get_author_name(self, author):
+        """
+        Args.
+        -----
+        - author: Author Object
+
+        Return.
+        --------
+        - Name String
+        """
+        if self.bibtex.language == 'EN':
+            name = author.name_en
+        else:
+            name = author.name_ja
+        return name
+
+        
+    def get_html_authors(self, authors):
+        """
+        Args.
+        -----
+        - authors: QuerySet of AuthorOrder
+
+        Return
+        ------
+        - HTML String of author's part
+        """
+        template = '<a href="{url}">{name}</a>'
+        html = ''
+        if len(authors) == 0:
+            return "None"
+        for i, author in enumerate(authors):
+            name = self.get_author_name(author.author)
+            url = reverse("dashboard:author_detail",
+                          kwargs={'pk':author.author.id})
+            html += template.format(url=url,name=name)
+            if not i+1 == len(authors):
+                html += ', '
+        return html
+    
+        
+    def get_template_DEFAULT(self):
+        html = (
+            '{authors}; '
+            '<a href="{url_bib}">"{title}"</a>, '
+            '(Default Style)'
+        )
+        return html        
+    
+
+
+
+# ==================
 #  Custom List View
 # ==================
-def bibtex_format_callback_list_simple(bibtex, *args, **kwargs):
-    title = bibtex.title_en if bibtex.title_en else bibtex.title_ja
-    url_bib = reverse("dashboard:bib_detail", kwargs={'pk':bibtex.id})
-
-    # Authors
-    html_authors_base = """
-    <a href="#">Author{}</a>
-    """
-    html_authors = [html_authors_base.format(i) for i in range(3)]
-    html_authors = ", ".join(html_authors)
+class BibtexFormatListDefault(BibtexFormatBase):
     
-    html = """
-    {authors}; <a href="{url_bib}">{title}</a>, 2017
-    """.format(
-        authors=html_authors, title=title, url_bib=url_bib
-    )
-    return mark_safe(html)
+    def get_template_INTPROC(self):
+        html = (
+            '{authors}; '
+            '<a href="{url_bib}">"{title}"</a>, '
+            '2017'
+        )
+        return html    
 
-
-
+    def get_template_JOURNAL(self):
+        html = (
+            '{authors}; '
+            '<a href="{url_bib}">"{title}"</a>, '
+            '2017 '
+            '(JOURNAL)'
+        )
+        return html
+    
 
 # ====================
 #  Custom Bibtex View
 # ====================
-def bibtex_format_callback_bibtex_simple(bibtex):
-
-    context = {
-        'title': bibtex.title_en if bibtex.title_en else bibtex.title_ja,
-        'authors': "Amagata, Daichi and Hara, Takahiro and Onizuka, Makoto",
-        'book': str(bibtex.book),
-        'vol': bibtex.volume,
-        'number': bibtex.number,
-        'pages': bibtex.page,
-        'year': bibtex.pub_date.year,
-        'publisher': bibtex.book.publisher,
-    }
+class BibtexFormatBibtexDefault(BibtexFormatBase):
     
-    html = [
-        '<pre class="mb-0" >',
-        '@article{{citekey,',
-        '  title={title},',
-        '  author={authors},',
-        '  journal={book},',
-        '  volume={vol},',
-        '  number={number},',
-        '  pages={pages},',
-        '  year={year},',
-        '  publisher={publisher},',
-        '}}',
-        '</pre>',
-    ]
-    html = "\n".join(html).format(**context)
-    return mark_safe(html)
+    def get_template_DEFAULT(self):
+        html = [
+            '<pre class="mb-0" >',
+            '@article{{citekey,',
+            '  title={title},',
+            '  author={authors},',
+            '  journal={book_title},',
+            '  volume={volume},',
+            '  number={number},',
+            '  pages={page},',
+            '  year={year},',
+            '  publisher={publisher},',
+            '}}',
+            '</pre>',
+        ]        
+        return "\n".join(html)
 
-
-
+    
 # ===================
 #  Custom Latex View
 # ===================
-def bibtex_format_callback_latex_simple(bibtex):
-
-    context = {
-        'title': bibtex.title_en if bibtex.title_en else bibtex.title_ja,
-        'authors': "Amagata, Daichi and Hara, Takahiro and Onizuka, Makoto",
-        'book': str(bibtex.book),
-        'vol': bibtex.volume,
-        'number': bibtex.number,
-        'pages': bibtex.page,
-        'year': bibtex.pub_date.year,
-        'publisher': bibtex.book.publisher,
-    }
+class BibtexFormatLatexDefault(BibtexFormatBase):    
+    def get_template_DEFAULT(self):
+        html = (
+            '\item {authors}, '
+            '"{title}", '
+            '{book_title}, '
+            '{volume}, {year},'
+        )  
+        return html
     
-    html = '\item {authors}, "{title}", {book}, {vol}, {year},'
-    html = html.format(**context)
-    return mark_safe(html)
 
 
 
 
+
+# ----------------------------------------------------------------------------
 
 # ===================
 #  Filter Func.
 # ===================
 @register.filter(name='bibtex_list_format')
-def bibtex_list_format(bibtex, func=bibtex_format_callback_list_simple, *args,**kwargs):
+def bibtex_list_format(bibtex, func=BibtexFormatListDefault, *args,**kwargs):
     """
     Args.
     -----
     - bibtex: core.Bibtex object
     """
-    return func(bibtex, *args, **kwargs)
-
+    return func(bibtex)()
 
 
 @register.filter(name='bibtex_bib_format')
-def bibtex_bib_format(bibtex, func=bibtex_format_callback_bibtex_simple, *args,**kwargs):
+def bibtex_bib_format(bibtex, func=BibtexFormatBibtexDefault, *args,**kwargs):
     """
     Args.
     -----
     - bibtex: core.Bibtex object
     """
-    return func(bibtex)
+    return func(bibtex)()
 
 
 @register.filter(name='bibtex_latex_format')
-def bibtex_latex_format(bibtex, func=bibtex_format_callback_latex_simple, *args,**kwargs):
+def bibtex_latex_format(bibtex, func=BibtexFormatLatexDefault, *args,**kwargs):
     """
     Args.
     -----
     - bibtex: core.Bibtex object
     """
-    return func(bibtex)
+    return func(bibtex)()
