b6120a56e306465534aa31b4f2eef4dbf05f89e2
DESKTOP-CA3IEJU\shinya <u361141e@gmail.com>
2018-10-24 15:01:30 +0900

    merge dev

---
+++ django/app/api/views_autocomplete.py
@@ -0,0 +1,17 @@
+from dal import autocomplete
+
+from core.models import Author
+
+class AutocompleteAuthor(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+        # Don't forget to filter out results depending on the visitor !
+        if not self.request.user.is_authenticated:
+            return Author.objects.none()
+
+        qs = Author.objects.all()
+        if self.q:
+            qs = qs.filter(name_en__istartswith=self.q)
+
+        return qs
+
+
