4643ec82036694dd5c076622385c887c30b732e4
DESKTOP-CA3IEJU\shinya <u361141e@gmail.com>
2018-10-24 14:33:55 +0900

    change template path

--- django/app/notification/alert.py
+++ django/app/notification/alert.py
@@ -10,15 +10,15 @@ def send_email_test():
     # 本文
     message = "The following papers have missing items.¥n"
 
-    """
+
     not_published_list = Bibtex.objects.filter(is_published=False)
-    mail_template = get_template('mail.txt')
+    mail_template = get_template('notification/mail_templates/mail.txt')
     for bib in not_published_list:
         context = {
             "bib": bib,
         }
         message = message + mail_template.render(context) + "¥n"
-    """
+
 
     # 送信元
     from_email = "test@test.com"
@@ -29,7 +29,7 @@ def send_email_test():
     recipient_list = [
         "mmde-m1@mmde-mail.ise.eng.osaka-u.ac.jp"
     ]
-    
+
     return send_mail(subject, message, from_email, recipient_list)
 
 """
