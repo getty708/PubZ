483f9b6b4cb96bb329372220d948d8baf8a5426c
HansWang <whx1994hans@gmail.com>
2018-11-01 22:00:38 +0900

    add route of card view

--- django/app/api/urls.py
+++ django/app/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from api import views
+from api import views_cards
 
 app_name = 'api'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),    
+    path('cards/', views_cards.render_cards, name='render_cards')
 ]
