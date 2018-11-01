6c0e4b6cebdea20bbabf929d2eb2ea99483f8cfa
HansWang <whx1994hans@gmail.com>
2018-11-01 23:35:09 +0900

    use perse_get_query_params to process query

--- django/app/api/views_cards.py
+++ django/app/api/views_cards.py
@@ -3,28 +3,24 @@ from django.shortcuts import get_object_or_404, render
 from django.views import generic
 
 from core.models import Author, Bibtex, Book, TagChain, Tag
+from dashboard.templatetags import utils_search as utils
 
 def render_cards(request):
-    if 'team' in request.GET:
-        team = request.GET.get('team')
-    query = sorted(request.GET.values())
-    print(query)
-
-    bibtex = Bibtex.objects.order_by('-pub_date')
+    bibtex,query_param_dic = utils.perse_get_query_params(request)
+    
     bib_to_render = []
     author_to_render = []
     for bib in bibtex:
-        if bib.tags.all()[0].name == team:
-            bib_to_render.append(bib)
-            authors = bib.authors.all()
-            num_of_authors = len(bib.authors.all())
-            author_str = ''
-            for i in range(num_of_authors):
-                if i == (num_of_authors - 1):
-                    author_str += 'and {}'.format(authors[i])
-                else:
-                    author_str += '{}, '.format(authors[i])
-            author_to_render.append(author_str)
+        bib_to_render.append(bib)
+        authors = bib.authors.all()
+        num_of_authors = len(bib.authors.all())
+        author_str = ''
+        for i in range(num_of_authors):
+            if i == (num_of_authors - 1):
+                author_str += 'and {}'.format(authors[i])
+            else:
+                author_str += '{}, '.format(authors[i])
+        author_to_render.append(author_str)
 
 
     return render(request,
