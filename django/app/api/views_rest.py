8648f955b61e118356b09090e58ceb7f6cba719d
Getty708 <yesyes708@gmail.com>
2018-11-21 00:42:43 +0900

    update permision fore REST APIs

--- django/app/api/views_rest.py
+++ django/app/api/views_rest.py
@@ -1,4 +1,5 @@
-# from django.contrib.auth.models import User, Group
+# from django.contrib.auth.models import User, Grou
+from django.contrib.auth.decorators import user_passes_test
 from rest_framework import viewsets, permissions, filters
 from users.models import User
 
@@ -9,15 +10,32 @@ from api import serializers
 from core.models import Author, AuthorOrder, Bibtex, Book
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 
+
+# -------------------------------------------------------------------
+class IsAdminOrReadOnly(permissions.BasePermission):
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    Assumes the model instance has an `owner` attribute.
+    """
+    def has_permission(self, request, view):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.        
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Instance must have an attribute named `owner`.
+        return request.user.is_superuser
+    
+
 # User
-# ------------------------------------------------------------------- 
+# -------------------------------------------------------------------
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = serializers.UserSerializer
-    authentication_classes = (SessionAuthentication, BasicAuthentication,)    
+    # permission_classes = (IsAdminOrReadOnly,)
+    authentication_classes = (SessionAuthentication, BasicAuthentication,)
 
 
 # -------------------------------------------------------------------
@@ -27,19 +45,19 @@ class AuthorViewSet(viewsets.ModelViewSet):
     """
     queryset = Author.objects.all().order_by('name_en')
     serializer_class = serializers.AuthorSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsAdminOrReadOnly,)
     authentication_classes = (SessionAuthentication,TokenAuthentication,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name_en','name_ja',)
 
-    
+
 class AuthorOrderViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
     queryset = AuthorOrder.objects.all().order_by('bibtex','order')
     serializer_class = serializers.AuthorOrderSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsAdminOrReadOnly,)    
     authentication_classes = (SessionAuthentication, TokenAuthentication,)
 
 
@@ -49,19 +67,19 @@ class BookViewSet(viewsets.ModelViewSet):
     """
     queryset = Book.objects.all().order_by('style', 'title')
     serializer_class = serializers.BookSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsAdminOrReadOnly,)    
     authentication_classes = (SessionAuthentication, TokenAuthentication,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('title',)
-    
 
+    
 class BibtexViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
     queryset = Bibtex.objects.all().order_by('title_en','title_ja',)
     serializer_class = serializers.BibtexSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsAdminOrReadOnly,)    
     authentication_classes = (SessionAuthentication, TokenAuthentication,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('title_en','title_ja')
--- django/app/app/settings.py
+++ django/app/app/settings.py
@@ -162,15 +162,13 @@ EMAIL_USE_TLS = True
 # REST API Settings
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication', 
         'rest_framework.authentication.TokenAuthentication',       
     ),    
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-        'rest_framework.permissions.IsAuthenticated',        
+        'rest_framework.permissions.IsAdminUser', 
     ],
     
     # Paging
