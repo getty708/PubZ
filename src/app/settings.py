a7cb110d4049f217dbaf6c65b9b1b93c100f9148
Naoya YOSHIMURA <yoshimura708x@gmail.com>
2020-09-21 13:15:08 +0900

    fix: change static file directory (#78)

--- src/app/settings.py
+++ src/app/settings.py
@@ -141,9 +141,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join('/persistent', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "core", "static"),
 )
 
 # Media files (Pictures)
